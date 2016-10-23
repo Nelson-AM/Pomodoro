0f32d429e1e0e0da9b983eed55feca51ab5a2d03
Nelson-AM <thenotionclub@gmail.com>
2016-10-23 19:52:09 +0200

    change deadline definition

--- timer.js
+++ timer.js
@@ -26,7 +26,7 @@ function initializeClock(id, endtime) {
 		daysSpan.innerHTML = t.days;
 		hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
 		minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-		secondsSpan.innerHTML = ('0' + t.minutes).slice(-2);
+		secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 		
 		if (t.total <= 0) {
 			clearInterval(timeinterval);
